cc0d932f0625c364e18e6ca4c31bb6ed96d02408
Ashish Sabharwal <AshishS@allenai.org>
2016-05-06 15:44:40 -0700

    add an MTurk-data based LookupLearner

---
+++ src/main/scala/org/allenai/ari/solvers/termselector/LookupLearner.scala
@@ -0,0 +1,34 @@
+package org.allenai.ari.solvers.termselector
+
+import org.allenai.ari.models.Question
+import org.allenai.ari.solvers.termselector.EssentialTermsUtils.Levenshtein
+
+/** A learner that simply looks up essential terms as annotated in MTurk data, if available. */
+class LookupLearner(confidenceThresholdOpt: Option[Double]) extends GenericLearner {
+
+  private val DEFAULT_CONFIDENCE_THRESHOLD = 0.9
+  private val confidenceThreshold = confidenceThresholdOpt.getOrElse(DEFAULT_CONFIDENCE_THRESHOLD)
+
+  lazy val questionEssentialTermScores = EssentialTermsSensors.allQuestions.map { q =>
+    q.rawQuestion -> q.essentialTermMap
+  }
+
+  /** Get MTurk annotated essential term scores for a given question. */
+  def getEssentialTermScores(aristoQuestion: Question): Map[String, Double] = {
+    val (_, termMap, minDistance) = questionEssentialTermScores.map {
+      case (q, termScores) => (q, termScores, Levenshtein.distance(q, aristoQuestion.rawQuestion))
+    }.minBy { case (_, _, distance) => distance }
+    val DISTANCE_THRESHOLD = 5
+    if (minDistance < DISTANCE_THRESHOLD) {
+      termMap.getOrElse(Map.empty)
+    } else {
+      throw new NoSuchElementException(s"Annotation not found for question: $aristoQuestion")
+    }
+  }
+
+  /** Get MTurk annotated essential term scores for a given question. */
+  def getEssentialTerms(aristoQuestion: Question): Seq[String] = {
+    val termScores = getEssentialTermScores(aristoQuestion)
+    termScores.collect { case (term, score) if score >= confidenceThreshold => term }.toSeq
+  }
+}
