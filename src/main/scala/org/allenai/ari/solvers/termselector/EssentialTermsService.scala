68fe4533ec6ec7ed545b1a7d4dfb45af10460f2f
Tushar Khot <tushark@allenai.org>
2016-05-11 18:14:07 -0700

    Use injection for the EssentialTermService

--- src/main/scala/org/allenai/ari/solvers/termselector/EssentialTermsService.scala
+++ src/main/scala/org/allenai/ari/solvers/termselector/EssentialTermsService.scala
@@ -3,20 +3,28 @@ package org.allenai.ari.solvers.termselector
 import org.allenai.ari.models.Question
 import org.allenai.common.Logging
 
-/** A service for identifying essential terms in Aristo questions. */
-class EssentialTermsService(
-    learnerType: String,
-    confidenceThreosholdOpt: Option[Double]
+import com.google.inject.Inject
+import com.google.inject.name.Named
+
+/** A service for identifying essential terms in Aristo questions.
+  * @param classifierType whether and how to identify and use essential terms in the model
+  * @param confidenceThreshold Threshold to call terms essential. If set to a negative value, use
+  * the classifier predictions directly
+  */
+class EssentialTermsService @Inject() (
+    @Named("essentialTerms.classifierType") val classifierType: String,
+    @Named("essentialTerms.confidenceThreshold") val confidenceThreshold: Double
 ) extends Logging {
 
-  logger.info(s"Initializing essential terms service with learner type: $learnerType")
-
-  /** Create a learner object. */
-  private val learner: EssentialTermsLearner = learnerType match {
-    case "Lookup" => new LookupLearner(None)
-    case "Baseline" => BaselineLearner.makeNewLearner(loadSavedModel = true)._2
-    case "Expanded" => ExpandedLearner.makeNewLearner(loadSavedModel = true)._4
-    case _ => throw new IllegalArgumentException(s"Unidentified learner type $learnerType")
+  /** Create a learner object. Lazy to avoid creating a learner if the service is not used. */
+  private lazy val learner: EssentialTermsLearner = {
+    logger.info(s"Initializing essential terms service with learner type: $classifierType")
+    classifierType match {
+      case "Lookup" => new LookupLearner(None)
+      case "Baseline" => BaselineLearner.makeNewLearner(loadSavedModel = true)._2
+      case "Expanded" => ExpandedLearner.makeNewLearner(loadSavedModel = true)._4
+      case _ => throw new IllegalArgumentException(s"Unidentified learner type $classifierType")
+    }
   }
 
   /** Get essential term scores for a given question. */
@@ -26,11 +34,11 @@ class EssentialTermsService(
 
   /** Get essential terms for a given question; use confidenceThreshold if provided. */
   def getEssentialTerms(aristoQ: Question): Seq[String] = {
-    confidenceThreosholdOpt match {
-      case Some(confidenceThreshold) =>
+    confidenceThreshold match {
+      case threshold if threshold >= 0 =>
         val termsWithScores = getEssentialTermScores(aristoQ)
-        termsWithScores.collect { case (term, score) if score >= confidenceThreshold => term }.toSeq
-      case None =>
+        termsWithScores.collect { case (term, score) if score >= threshold => term }.toSeq
+      case _ =>
         learner.getEssentialTerms(aristoQ)
     }
   }
