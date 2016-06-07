6d31de835c66331970be2bd3874c4f9b6da17982
danyaljj <khashab2@illinois.edu>
2016-06-07 15:11:20 -0700

    adding probability scores to the baseline classifier.

--- src/main/scala/org/allenai/ari/solvers/termselector/CountClassifier.scala
+++ src/main/scala/org/allenai/ari/solvers/termselector/CountClassifier.scala
@@ -1,6 +1,7 @@
 package org.allenai.ari.solvers.termselector
 
 import edu.illinois.cs.cogcomp.lbj.pos.POSBaselineLearner
+import edu.illinois.cs.cogcomp.lbjava.classify.ScoreSet
 
 import scala.collection.JavaConverters._
 
@@ -14,4 +15,19 @@ class CountClassifier extends POSBaselineLearner {
       countsMap.asScala.toList.maxBy(_._2)._1
     }
   }
+
+  /** get probabiltiy score for the baselines */
+  override def scores(example: AnyRef): ScoreSet = {
+    val scoreSet = new ScoreSet()
+    val form = extractor.discreteValue(example)
+    val countsMap = table.get(form).asScala
+    val countSum = countsMap.values.foldRight(0)(_ + _)
+    if (countsMap.nonEmpty) {
+      countsMap.foreach { case (label, count) => scoreSet.put(label, count.toDouble / countSum) }
+    } else {
+      scoreSet.put("UNKNOWN", 1)
+    }
+    scoreSet
+  }
+
 }
