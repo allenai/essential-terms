4d336accf519d1b272d97a8ee2d283f079deb900
Ashish Sabharwal <AshishS@allenai.org>
2016-07-01 16:38:55 -0700

    update spec test

--- src/test/scala/org/allenai/ari/solvers/termselector/EssentialTermsSpec.scala
+++ src/test/scala/org/allenai/ari/solvers/termselector/EssentialTermsSpec.scala
@@ -9,18 +9,20 @@ import org.allenai.common.testkit.UnitSpec
 class EssentialTermsSpec extends UnitSpec {
   //TODO: ignored due to some weird behavior in Semaphore. Address it in future.
   "Lemma baseline " should "should correctly work and have at least 74 F1" ignore {
-    val (baselineDataModelTrain, baselineLearnersTrain) = BaselineLearner.makeNewLearners(LoadFromDatastore, "train")
+    val (baselineDataModelTrain, baselineLearnersTrain) =
+      BaselineLearners.makeNewLearners(LoadFromDatastore, "train")
     // load the data into the model
     baselineDataModelTrain.essentialTermTokens.populate(testConstituents, train = false)
     val evaluator = new Evaluator(baselineLearnersTrain.lemma)
     val scoreMap = evaluator.test(testConstituents, Constants.LEMMA_BASELINE_THRESHOLD, 1.0)
     assert(scoreMap(Constants.IMPORTANT_LABEL)._1 >= 0.74)
   }
-  1
+
   //TODO: ignored due to some weird behavior in Semaphore. Address it in future.
   "Expanded classifier " should " should correctly work and have at least 80 F1" ignore {
     val salienceBaselines = SalienceLearner.makeNewLearners()
-    val (baselineDataModel, baselineClassifiers) = BaselineLearner.makeNewLearners(LoadFromDatastore, "dev")
+    val (baselineDataModel, baselineClassifiers) =
+      BaselineLearners.makeNewLearners(LoadFromDatastore, "dev")
     val (expandedDataModel, expandedLearner) = ExpandedLearner.makeNewLearner(LoadFromDatastore, "SVM",
       baselineClassifiers, baselineDataModel, salienceBaselines)
     // load the data into the model
