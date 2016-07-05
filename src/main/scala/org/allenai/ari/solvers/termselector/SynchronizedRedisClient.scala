661a2f0383aad357d75e9398ca1b7ead27bb7925
danyaljj <khashab2@illinois.edu>
2016-07-05 13:49:22 -0700

    removing redisClientOpt.

--- src/main/scala/org/allenai/ari/solvers/termselector/SynchronizedRedisClient.scala
+++ src/main/scala/org/allenai/ari/solvers/termselector/SynchronizedRedisClient.scala
@@ -5,51 +5,42 @@ import org.allenai.common.Logging
 
 /** a thread-safe wrapper for redis-client */
 class SynchronizedRedisClient(host: String = "localhost") extends Logging {
-  private lazy val redisClientOpt = try {
-    Some(new RedisClient(host, 6379))
+  private lazy val redisClient = try {
+    new RedisClient(host, 6379)
   } catch {
     case e: RuntimeException =>
       throw new Exception("Redis is not running, although it is set to be used in \"application.conf\" . . . ")
   }
 
   def redisGet(key: String): Option[String] = {
-    redisClientOpt.flatMap {
-      redisClient =>
-        try {
-          this.synchronized(redisClient.get(key))
-        } catch {
-          case e: Exception => {
-            e.printStackTrace()
-            throw new Exception(s"Fetching information from redis failed for key: $key!")
-          }
-        }
+    try {
+      this.synchronized(redisClient.get(key))
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        throw new Exception(s"Fetching information from redis failed for key: $key!")
+      }
     }
   }
 
   def redisSet(key: String, value: String): Unit = {
-    redisClientOpt.map {
-      redisClient =>
-        try {
-          this.synchronized(redisClient.set(key, value))
-        } catch {
-          case e: Exception => {
-            e.printStackTrace()
-            throw new Exception(s"Setting information in redis failed for key: $key")
-          }
-        }
+    try {
+      this.synchronized(redisClient.set(key, value))
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        throw new Exception(s"Setting information in redis failed for key: $key")
+      }
     }
   }
 
   def keys(): Option[List[Option[String]]] = {
-    redisClientOpt.flatMap {
-      redisClient =>
-        try {
-          this.synchronized(redisClient.keys())
-        } catch {
-          case e: Exception =>
-            e.printStackTrace()
-            throw new Exception("Fetching all keys from redis failed!")
-        }
+    try {
+      this.synchronized(redisClient.keys())
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        throw new Exception("Fetching all keys from redis failed!")
     }
   }
 }
