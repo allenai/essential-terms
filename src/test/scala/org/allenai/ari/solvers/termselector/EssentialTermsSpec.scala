a8cf0d3a9491df9e9dec21d8b75706e898963d57
khashab2 <khashab2@illinois.edu>
2016-08-07 19:57:12 -0700

    adding tests for the service.

--- src/test/scala/org/allenai/ari/solvers/termselector/EssentialTermsSpec.scala
+++ src/test/scala/org/allenai/ari/solvers/termselector/EssentialTermsSpec.scala
@@ -1,5 +1,6 @@
 package org.allenai.ari.solvers.termselector
 
+import edu.illinois.cs.cogcomp.lbjava.classify.TestDiscrete
 import org.allenai.ari.solvers.termselector.Sensors._
 import org.allenai.ari.solvers.termselector.evaluation.Evaluator
 import org.allenai.ari.solvers.termselector.learners._
@@ -7,51 +8,84 @@ import org.allenai.common.testkit.UnitSpec
 
 /** Test overall functionality of the TableILP solver */
 class EssentialTermsSpec extends UnitSpec {
-  "Lemma baseline " should "should correctly work and have at least 74 F1" in {
-    val (baselineDataModelTrain, baselineLearnersTrain) =
-      BaselineLearners.makeNewLearners(LoadFromDatastore, "train")
-    // load the data into the model
-    baselineDataModelTrain.essentialTermTokens.populate(testConstituents, train = false)
-    val evaluator = new Evaluator(baselineLearnersTrain.lemma)
-    val scoreMap = evaluator.test(testConstituents, Constants.LEMMA_BASELINE_THRESHOLD, 1.0)
-    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
-    f1Score should be(0.74 +- 0.02)
+  //  "Lemma baseline " should "should correctly work and have at least 74 F1" in {
+  //    val (baselineDataModelTrain, baselineLearnersTrain) =
+  //      BaselineLearners.makeNewLearners(LoadFromDatastore, "train")
+  //    // load the data into the model
+  //    baselineDataModelTrain.essentialTermTokens.populate(testConstituents, train = false)
+  //    val evaluator = new Evaluator(baselineLearnersTrain.lemma)
+  //    val scoreMap = evaluator.test(testConstituents, Constants.LEMMA_BASELINE_THRESHOLD, 1.0)
+  //    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
+  //    f1Score should be(0.74 +- 0.02)
+  //  }
+  //
+  //  "Salience baseline " should "should correctly work and have at least 67 F1" in {
+  //    val maxSalienceBaseline = SalienceLearner.makeNewLearners().max
+  //    val evaluator = new Evaluator(maxSalienceBaseline)
+  //    val scoreMap = evaluator.test(testConstituents, Constants.LEMMA_BASELINE_THRESHOLD, 1.0)
+  //    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
+  //    f1Score should be(0.67 +- 0.02)
+  //  }
+  //
+  //  "Expanded classifier " should " should correctly work and have at least 80 F1" in {
+  //    val salienceBaselines = SalienceLearner.makeNewLearners()
+  //    val (baselineDataModel, baselineClassifiers) =
+  //      BaselineLearners.makeNewLearners(LoadFromDatastore, "dev")
+  //    val (expandedDataModel, expandedLearner) = ExpandedLearner.makeNewLearner(LoadFromDatastore, "SVM",
+  //      baselineClassifiers, baselineDataModel, salienceBaselines)
+  //    // load the data into the model
+  //    expandedDataModel.essentialTermTokens.populate(testConstituents, train = false)
+  //    val evaluator = new Evaluator(expandedLearner)
+  //    val scoreMap = evaluator.test(testConstituents, Constants.EXPANDED_LEARNER_THRESHOLD, 1.0)
+  //    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
+  //    f1Score should be(0.80 +- 0.02)
+  //  }
+  //
+  //  "Expanded classifier for direct answer questions " should " should correctly work and have at least 79.5 F1" in {
+  //    val (baselineDataModel, baselineClassifiers) =
+  //      BaselineLearners.makeNewLearners(LoadFromDatastore, "dev")
+  //    val dummySalienceLearner = new DummySalienceLearner(baselineDataModel)
+  //    val salienceBaselines = SalienceLearners(dummySalienceLearner, dummySalienceLearner)
+  //    val (expandedDataModel, expandedLearner) = ExpandedLearner.makeNewLearner(LoadFromDatastore, "SVM-directAnswer",
+  //      baselineClassifiers, baselineDataModel, salienceBaselines)
+  //    // load the data into the model
+  //    expandedDataModel.essentialTermTokens.populate(testConstituents, train = false)
+  //    val evaluator = new Evaluator(expandedLearner)
+  //    val scoreMap = evaluator.test(testConstituents, Constants.EXPANDED_LEARNER_THRESHOLD_DIRECT_ANSWER, 1.0)
+  //    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
+  //    f1Score should be(0.795 +- 0.02)
+  //  }
+
+  def getServiceF1GivenLearnerType(classifierType: String, classifierMode: String): Double = {
+    val lookupLearner = new InjectedLearnerAndThreshold(classifierType, classifierMode)
+    val lookupLearnerService = new EssentialTermsService(lookupLearner, false)
+
+    val lemmaLearner = new InjectedLearnerAndThreshold("LemmaBaseline", "")
+    val lemmaLearnerService = new EssentialTermsService(lemmaLearner, false)
+
+    val tester = new TestDiscrete
+    allQuestions.foreach { q =>
+      val (goldEssentialTerms, goldEssentialTermScoreMap) = lookupLearnerService.getEssentialTermsAndScores(q.aristoQuestion)
+      val predictedTerms = lemmaLearnerService.getEssentialTerms(q.aristoQuestion)
+      goldEssentialTermScoreMap.keys.foreach { term =>
+        tester.reportPrediction(predictedTerms.contains(term).toString, goldEssentialTerms.contains(term).toString)
+      }
+    }
+    tester.getF1("true")
   }
 
-  "Salience baseline " should "should correctly work and have at least 67 F1" in {
-    val maxSalienceBaseline = SalienceLearner.makeNewLearners().max
-    val evaluator = new Evaluator(maxSalienceBaseline)
-    val scoreMap = evaluator.test(testConstituents, Constants.LEMMA_BASELINE_THRESHOLD, 1.0)
-    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
-    f1Score should be(0.67 +- 0.02)
+  "Essentialterms service for lemma-baseline" should "work" in {
+    val f1Score = getServiceF1GivenLearnerType("Lookup", "")
+    f1Score should be(0.786 +- 0.02)
   }
 
-  "Expanded classifier " should " should correctly work and have at least 80 F1" in {
-    val salienceBaselines = SalienceLearner.makeNewLearners()
-    val (baselineDataModel, baselineClassifiers) =
-      BaselineLearners.makeNewLearners(LoadFromDatastore, "dev")
-    val (expandedDataModel, expandedLearner) = ExpandedLearner.makeNewLearner(LoadFromDatastore, "SVM",
-      baselineClassifiers, baselineDataModel, salienceBaselines)
-    // load the data into the model
-    expandedDataModel.essentialTermTokens.populate(testConstituents, train = false)
-    val evaluator = new Evaluator(expandedLearner)
-    val scoreMap = evaluator.test(testConstituents, Constants.EXPANDED_LEARNER_THRESHOLD, 1.0)
-    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
-    f1Score should be(0.80 +- 0.02)
+  "Essentialterms service for salience-baseline" should "work" in {
+    val f1Score = getServiceF1GivenLearnerType("MaxSalience", "")
+    f1Score should be(0.871 +- 0.02)
   }
 
-  "Expanded classifier for direct answer questions " should " should correctly work and have at least 79.5 F1" in {
-    val (baselineDataModel, baselineClassifiers) =
-      BaselineLearners.makeNewLearners(LoadFromDatastore, "dev")
-    val dummySalienceLearner = new DummySalienceLearner(baselineDataModel)
-    val salienceBaselines = SalienceLearners(dummySalienceLearner, dummySalienceLearner)
-    val (expandedDataModel, expandedLearner) = ExpandedLearner.makeNewLearner(LoadFromDatastore, "SVM-directAnswer",
-      baselineClassifiers, baselineDataModel, salienceBaselines)
-    // load the data into the model
-    expandedDataModel.essentialTermTokens.populate(testConstituents, train = false)
-    val evaluator = new Evaluator(expandedLearner)
-    val scoreMap = evaluator.test(testConstituents, Constants.EXPANDED_LEARNER_THRESHOLD_DIRECT_ANSWER, 1.0)
-    val f1Score = scoreMap(Constants.IMPORTANT_LABEL)._1
-    f1Score should be(0.795 +- 0.02)
+  "Essentialterms service for expanded-classifier" should "work" in {
+    val f1Score = getServiceF1GivenLearnerType("Expanded", "SVM")
+    f1Score should be(0.897 +- 0.02)
   }
 }
